193f0d3fc056fe329ff3db74e76cb37416d704a7
terromn <terromn@users.noreply.github.com>
2025-07-12 04:43:51 -0600

    chore: fix start cmd in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,7 +10,15 @@ export default defineConfig({
     tailwind(),
     react()
   ],
-  output: 'static',
+  server: {
+    host: '0.0.0.0'
+  },
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   build: {
     inlineStylesheets: 'auto',
   },
